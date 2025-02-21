d506138adeffe9545e3e5631171df9e3e34e955d
Daryl Adrien E <Edaryl2705@gmail.com>
2025-02-21 21:51:27 +0530

    Update list_projects.py

--- list_projects.py
+++ list_projects.py
@@ -2,9 +2,9 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-url = "https://daryl.atlassian.net/rest/api/3/project"
+url = "https://xxx.atlassian.net/rest/api/3/project"
 
-auth = HTTPBasicAuth("daryl.adriene111@ptuniv.edu.in", "ATATT3xFfGF0OcwJdUUhQEVWePEuoaS7oAXg7yk7WGy1Y3iDOKysC-924ZwI2FeV5WOTv0M6i7XZJwVAa6JFKpPaxY4_KqQD0cgzQctdij8ONZzKqy4W9gs_x5w-qG-Pcrq0Y0L-HxjdqcF8BbgfcHbfohInVw5ATGrFs4XxFt-JRCGw-EdNAGc=912C5540")
+auth = HTTPBasicAuth("", "")
 
 headers = {
   "Accept": "application/json"
